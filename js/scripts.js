06caa13bfee0ff2b95cba2544ce597c90f704c4d
Bagus Indrayana <bagusindrayanaindo@gmail.com>
2021-11-14 10:16:36 +0800

    tambah kapal terbang untuk modal promo

--- js/scripts.js
+++ js/scripts.js
@@ -12,9 +12,10 @@ const clock = new THREE.Clock()
 const scene = new THREE.Scene();
 var aspect = window.innerWidth / window.innerHeight;
 var d = 20;
-const camera = new THREE.OrthographicCamera( - d * aspect, d * aspect, d, - d, 1, 1000 );
-camera.position.set( 10, 10,10 );
+const camera = new THREE.OrthographicCamera( - d * aspect, d * aspect, d, - d, 0.01, 1000 );
+camera.position.set( 15, 15,15 );
 camera.lookAt( scene.position ); 
+var openModal = false;
 
 //diplay block modal
 window.addEventListener("load", function(){
@@ -22,12 +23,12 @@ window.addEventListener("load", function(){
 });
 
 //pencahayaan
-const hemiLight = new THREE.HemisphereLight( 0xffffff,0.5);
-hemiLight.position.set( 0, 3, 0 );
-scene.add( hemiLight );
+// const hemiLight = new THREE.HemisphereLight( 0xffffff,0.5);
+// hemiLight.position.set( 0, 3, 0 );
+// scene.add( hemiLight );
 
 const dirLight = new THREE.DirectionalLight( 0xffffff );
-dirLight.position.set( -2, 5, 12 );
+dirLight.position.set( -3, 22, 12 );
 dirLight.intensity = 1;
 dirLight.castShadow = true;
 var side = 20;
@@ -35,11 +36,17 @@ dirLight.shadow.camera.top = side;
 dirLight.shadow.camera.bottom = -side;
 dirLight.shadow.camera.left = side;
 dirLight.shadow.camera.right = -side;
+dirLight.shadow.camera.far = 1000;
 
 scene.add( dirLight );
 
-//uncomment jika ingin menampilkan helper bagian mana saja yang terkena bayangan
+const light = new THREE.AmbientLight( 0x404040 ); // soft white light
+scene.add( light );
 
+// const fog = new THREE.Fog( 0x3f7b9d, 0, 60 );
+// scene.fog = fog;
+
+//uncomment jika ingin menampilkan helper bagian mana saja yang terkena bayangan
 // var shadowHelper = new THREE.CameraHelper( dirLight.shadow.camera );
 // scene.add( shadowHelper );
 
@@ -85,6 +92,8 @@ manager.onLoad = function ( ) {
     document.addEventListener( 'mousemove', onDocumentMouseMove, false );
     document.addEventListener( 'mousedown', onDocumentMouseDown, false );
 
+    gsap.to("#model-3d canvas", {duration: 1, opacity:1});
+
 };
 
 const loader = new GLTFLoader(manager);
@@ -100,7 +109,7 @@ loader.load( 'model/city_iso.gltf', function ( gltf ) {
             if((name == "ground" || name == "road" || name == "white")){
                 object.receiveShadow = true;
             } else {
-                // object.receiveShadow = true;
+                object.receiveShadow = true;
                 if(name == "tree" || name == "tree_leaf"){
                     
                 } else {
@@ -141,8 +150,45 @@ loader.load( 'model/mobil_iso.gltf', function ( gltf ) {
     // gltf.scene.position.set(0,0,0);
     gltf.scene.traverse( function ( object ) {
         if(object.isMesh){
+            object.receiveShadow = true;
             object.castShadow = true;
         }
+            
+
+    } );
+
+    //menjalankan animasi mobil
+    var animations = gltf.animations;
+    for (let i = 0; i < animations.length; i++) {
+        const animation = animations[i];
+        mixers.push( new THREE.AnimationMixer(gltf.scene) );
+        
+        var action = mixers[mixers.length-1].clipAction( animation); // access first animation clip
+        // action.loop = THREE.LoopOnce;
+        // action.clampWhenFinished = true;
+        action.play();
+    
+        
+    }
+    
+    scene.add(gltf.scene);
+    
+}, undefined, function ( error ) {
+
+    console.error( error );
+
+} );
+
+
+//load model kapal terbang
+loader.load( 'model/kapal_terbang_iso.gltf', function ( gltf ) {
+    
+    // gltf.scene.position.set(0,0,0);
+    gltf.scene.traverse( function ( object ) {
+        if(object.isMesh){
+            object.castShadow = true;
+        }
+        buildings.push(object);
             
 
     } );
@@ -204,6 +250,8 @@ function animate() {
             }
         }
 
+        
+
     } );
     
     if(controls){
@@ -245,7 +293,7 @@ function onDocumentMouseMove(event) {
         for ( var i = 0;  intersects.length > 0 && i < intersects.length; i++)
         {   
             
-            if(intersects[0].object)
+            if(intersects[0].object && !openModal)
             {   
                 var find = false;
                 buildings.forEach(b => {
@@ -254,9 +302,11 @@ function onDocumentMouseMove(event) {
                         INSPECTED = intersects[0].object.parent;
                         if(INSPECTED != undefined){
                             find = true;
+                           
+                            
                             scaleUp();
                             
-                        } 
+                        }
                     }
                 });
                 hover = find;
@@ -265,9 +315,16 @@ function onDocumentMouseMove(event) {
                 
     
         }
+
+        if(hover){
+            document.body.style.cursor = 'pointer';
+        } else {
+            document.body.style.cursor = 'default';
+        }
         
     } else {
         hover = false;
+        document.body.style.cursor = 'default';
     }
 
 }
@@ -295,11 +352,16 @@ function onDocumentMouseDown(event) {
                         if(intersects[0].object.parent.name.includes("building")){
                             document.getElementById("image").src = "https://images.unsplash.com/photo-1486406146926-c627a92ad1ab?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1470&q=80";
                             document.getElementById("content").innerHTML = "Di jual perkantoran Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus quam eligendi voluptatibus aspernatur quas, optio reiciendis rem autem. Et tenetur illum sapiente non exercitationem aspernatur distinctio ipsum molestiae amet odio.";
-                        } else {
+                        } else if(intersects[0].object.parent.name.includes("kapal_terbang")){
+                            document.getElementById("image").src = "https://images.unsplash.com/photo-1580828343064-fde4fc206bc6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80";
+                            document.getElementById("content").innerHTML = "Diskon akhir tahun Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus quam eligendi voluptatibus aspernatur quas, optio reiciendis rem autem. Et tenetur illum sapiente non exercitationem aspernatur distinctio ipsum molestiae amet odio.";
+                            
+                        }else {
                             document.getElementById("image").src = "https://images.unsplash.com/photo-1576941089067-2de3c901e126?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1578&q=80";
                             document.getElementById("content").innerHTML = "Di jual rumah Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus quam eligendi voluptatibus aspernatur quas, optio reiciendis rem autem. Et tenetur illum sapiente non exercitationem aspernatur distinctio ipsum molestiae amet odio.";
                         }
                         document.getElementById("modal").classList.add("show");
+                        openModal = true;
                     }
                 });
                 
@@ -315,4 +377,5 @@ function onDocumentMouseDown(event) {
 
 document.getElementById("close-modal").addEventListener("click", function(){
     document.getElementById("modal").classList.remove("show");
+    openModal = false;
 });
